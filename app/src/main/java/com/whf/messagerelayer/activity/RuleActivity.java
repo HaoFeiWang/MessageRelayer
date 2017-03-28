37fe5c0c33e568af2416e738b61fd1ec9a39ad6d
haofeiwang <292265806@qq.com>
2017-03-28 12:47:05 +0800

    创建自定义转发规则的Activity

---
+++ app/src/main/java/com/whf/messagerelayer/activity/RuleActivity.java
@@ -0,0 +1,15 @@
+package com.whf.messagerelayer.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.whf.messagerelayer.R;
+
+public class RuleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rule);
+    }
+}
